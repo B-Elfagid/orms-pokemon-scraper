70ef68effab5b6a96b6814068e4489aea778b5e1
B-Elfagid <betty_elfagid@yahoo.co.uk>
2020-12-30 14:59:12 +0000

    commit tree 48ea1beda39dfede4985b0b5bffb083e73839ec1

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,24 @@
+
 class Pokemon
+
+attr_accessor :name, :type, :db
+attr_reader :id
+
+
+def initialize(id: nil, name:, type:, db:)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
+end 
+
+def self.save(name, type, db)
+    db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", name, type)
+end 
+
+def self.find(id, db)
+    new_database = db.execute("SELECT * FROM pokemon WHERE id = ?;", id).flatten
+    Pokemon.new(id: new_database[0], name: new_database[1], type: new_database[2], db: db)
+end 
+
 end
